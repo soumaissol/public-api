45a7b7b601c3afe0963a0d0c9ba47d13c1c055e6
Joao Araujo <jpdejavite@gmail.com>
2023-07-18 22:55:26 -0300

    feat: 100% coverage for unit test

--- __tests__/unit/application/usecase/get-power-distributors.test.ts
+++ __tests__/unit/application/usecase/get-power-distributors.test.ts
@@ -2,6 +2,7 @@ import InvalidInput from '../../../../src/application/errors/invalid-input';
 import EmptyInput from '../../../../src/application/errors/empty-input';
 import FakeSolarEnergyGateway from '../../../../src/infra/solar-energy-gateway/fake-solar-energy-gateway';
 import GetPowerDistributors from '../../../../src/application/usecase/get-power-distributors';
+import SolarEnergyGateway from '../../../../src/infra/solar-energy-gateway/solar-energy-gateway';
 
 describe('Test GetPowerDistributors usecase', () => {
   it('should return error when input is null', async () => {
@@ -14,11 +15,11 @@ describe('Test GetPowerDistributors usecase', () => {
     }
   });
 
-  it('should return error when input is empty object', async () => {
+  it('should return error when input is broken json', async () => {
     try {
       const getPowerDistributors = new GetPowerDistributors(new FakeSolarEnergyGateway());
 
-      await getPowerDistributors.execute(JSON.stringify({}));
+      await getPowerDistributors.execute('}');
     } catch (err) {
       expect(err).toEqual(new InvalidInput('invalid zip', 'invalid_zip'));
     }
@@ -36,14 +37,24 @@ describe('Test GetPowerDistributors usecase', () => {
 
   it('should return listing when all is valid', async () => {
     const getPowerDistributors = new GetPowerDistributors(new FakeSolarEnergyGateway());
-    const result = await getPowerDistributors.execute(JSON.stringify({
-      powerDistributorId: 1,
-      email: 'user@email.com', energyConsumption: 100, zip: '12323123',
-    }));
+    const result = await getPowerDistributors.execute(JSON.stringify({ zip: '12323123' }));
     expect(result).toEqual({
       addressName: 'Rua Rubens Meireles', city: 'São Paulo', state: 'São Paulo',
       sa: 'SP', powerDistributors: [{ id: 115, name: 'Cedrap', price: 0.8 },
       { id: 116, name: 'Cedri', price: 0.74 }],
     });
+  });
+
+  it('should return null when when energy solar gateway fails', async () => {
+    const solarEnergyGatewayMock: SolarEnergyGateway = {
+      calculateInstallationSimulation: jest.fn(),
+      getPowerDistributorListing: jest.fn().mockImplementationOnce(() => {
+        throw new Error('unknow error');
+      }),
+    };
+
+    const getPowerDistributors = new GetPowerDistributors(solarEnergyGatewayMock);
+    const result = await getPowerDistributors.execute(JSON.stringify({ zip: '12323123' }));
+    expect(result).toBeNull();
   });
 });
